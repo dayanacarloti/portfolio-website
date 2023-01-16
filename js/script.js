5245719e74b6e1a76a9e4f8f79457269fb52abcb
dayanacarloti <dayanacarloti@gmail.com>
2023-01-16 12:43:42 +0100

    Header.html
    html, css, script, jquery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 <script>
   alert("Hi! Welcome to My Portfolio Site");
 </script>
-<script src="js/scripts.js"></script>
+
+function menuToggle() {
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
